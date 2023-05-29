92baaaab2e8474601dc51ccd439792858c55f73b
Rickyanto Wangsa Mulya <rickyanto.wangsa@ui.ac.id>
2023-05-30 02:26:53 +0700

    chore: run prisma migration

---
+++ prisma/migrations/20230529192616_create_user/migration.sql
@@ -0,0 +1,27 @@
+-- CreateEnum
+CREATE TYPE "Genders" AS ENUM ('M', 'F');
+
+-- CreateEnum
+CREATE TYPE "ActivityLevels" AS ENUM ('SD', 'LA', 'MA', 'VA', 'SA');
+
+-- CreateEnum
+CREATE TYPE "Goals" AS ENUM ('LW', 'MW', 'GW');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "firebaseId" TEXT NOT NULL,
+    "height" INTEGER NOT NULL,
+    "weight" INTEGER NOT NULL,
+    "gender" "Genders" NOT NULL,
+    "age" INTEGER NOT NULL,
+    "activityLevel" "ActivityLevels" NOT NULL,
+    "goal" "Goals" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_firebaseId_key" ON "User"("firebaseId");
