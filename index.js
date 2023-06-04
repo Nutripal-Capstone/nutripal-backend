1489aa9d2ffa0fbe9555e11c8a488fd1fcb622b4
Aprilia Khoirunnisa <78308985+choconutzy@users.noreply.github.com>
2023-06-05 05:49:08 +0700

    change port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ dotenv.config();
 import express from "express";
 import authRouter from "./routes/authRoutes.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.json());
