f8fdf4e4a38328d8d18924124d4c7ea1a7290c53
Aprilia Khoirunnisa <aprilianissa112@gmail.com>
2023-06-05 10:57:02 +0700

    add test

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ dotenv.config();
 
 import express from "express";
 import authRouter from "./routes/authRoutes.js";
+import prisma from "./config/prisma.js";
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.json());
+app.get("/", async(req, res) => {
+  try {
+    await prisma.$connect();
+    res.status(200).json({
+      "message" : "Hello Nutripal User! Database Connection Successful"
+    })
+    
+  } catch (error) {
+    console.error(error)
+  }
+});
 app.use("/auth", authRouter);
 
 app.listen(PORT, () =>
