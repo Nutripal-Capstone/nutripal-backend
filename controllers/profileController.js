a5d85a18f2b7a05c9188abecfe4c940e3e62d573
Aprilia Khoirunnisa <aprilianissa112@gmail.com>
2023-06-07 07:18:23 +0700

    add profile controller

---
+++ controllers/profileController.js
@@ -0,0 +1,44 @@
+import prisma from "../config/prisma.js";
+
+export const getProfile = async (req, res) => {
+    const userId = req.user.id
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+              id: userId
+            },
+          });
+        res.status(200).json({
+            "success": true,
+            "data" : user,
+            "message" : "Get profile user successfull"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            data: { error: error.message },
+            message: "Error fetching profile data.",
+          });
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    const newData = req.body
+    try {
+        const user = await prisma.user.update({
+            where:{userId: req.user.id},
+            data: newData
+        })
+        res.status(200).json({
+            "success": true,
+            "data" : user,
+            "message" : "Update profile user successfull"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            data: { error: error.message },
+            message: "Error update profile data.",
+          });
+    }
+}
