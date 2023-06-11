a3bab1d8198c552c5c083b90793714de13c1c128
Rickyanto Wangsa Mulya <rickyanto.wangsa@ui.ac.id>
2023-06-12 03:41:11 +0700

    feat: implement food detail

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -27,31 +27,18 @@ export const searchFood = async (req, res) => {
     };
 
     if (response.data.foods_search.results) {
-      const properties = [
-        'saturated_fat', 'polyunsaturated_fat', 'monounsaturated_fat', 'trans_fat',
-        'cholesterol', 'sodium', 'potassium', 'fiber', 'sugar', 'added_sugars', 
-        'vitamin_d', 'vitamin_a', 'vitamin_c', 'calcium', 'iron'
-      ];
-      
       response.data.foods_search.results.food.forEach((foodItem) => {
         foodItem.servings.serving.forEach((serving) => {
-          const data = {
+          output.data.push({
             food_id: foodItem.food_id,
             serving_id: serving.serving_id,
             food_name: foodItem.food_name,
-            food_type: foodItem.food_type,
             serving_description: serving.serving_description,
             calories: parseFloat(serving.calories),
             carbohydrate: parseFloat(serving.carbohydrate),
             protein: parseFloat(serving.protein),
             fat: parseFloat(serving.fat),
-          };
-          
-          properties.forEach((prop) => {
-            data[prop] = serving[prop] !== undefined ? parseFloat(serving[prop]) : null;
           });
-          
-          output.data.push(data);
         });
       });
     }
@@ -63,5 +50,82 @@ export const searchFood = async (req, res) => {
       data: null,
       message: "Error searching for food",
     });
+  }
+};
+
+export const detailFood = async (req, res) => {
+  const { food_id, recipe_id } = req.query;
+
+  try {
+    const response = await axios.get(
+      `https://platform.fatsecret.com/rest/server.api?method=food.get.v3&food_id=${food_id}`,
+      {
+        headers: { Authorization: `Bearer ${req.accessToken}` },
+      }
+    );
+
+    const { data } = response;
+    const {
+      servings: { serving },
+    } = data;
+
+    const selectedServing = serving.find(
+      (item) => item.serving_id === recipe_id
+    );
+
+    const properties = [
+      "saturated_fat",
+      "polyunsaturated_fat",
+      "monounsaturated_fat",
+      "trans_fat",
+      "cholesterol",
+      "sodium",
+      "potassium",
+      "fiber",
+      "sugar",
+      "added_sugars",
+      "vitamin_d",
+      "vitamin_a",
+      "vitamin_c",
+      "calcium",
+      "iron",
+    ];
+
+    let result = null;
+
+    if (selectedServing) {
+      result = {
+        food_id: data.food_id,
+        serving_id: selectedServing.serving_id,
+        food_name: data.food_name,
+        serving_description: selectedServing.measurement_description,
+        calories: parseFloat(selectedServing.calories),
+        carbohydrate: parseFloat(selectedServing.carbohydrate),
+        protein: parseFloat(selectedServing.protein),
+        fat: parseFloat(selectedServing.fat),
+      };
+
+      properties.forEach((prop) => {
+        result[prop] =
+          selectedServing[prop] !== undefined
+            ? parseFloat(selectedServing[prop])
+            : null;
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: `Success getting detail for ${data.food_name}.`,
+      data: result,
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Error getting food detail.",
+        data: null,
+      });
   }
 };
---
+++ routes/foodRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import verifyJWT from "../middleware/verifyJWT.js";
+import { searchFood, detailFood } from "../controllers/searchController.js";
+import validateAccessToken from "../middleware/validateAccessToken.js";
+
+const foodRouter = express.Router();
+
+foodRouter.get("/search", verifyJWT, validateAccessToken, searchFood);
+foodRouter.get("/search", verifyJWT, validateAccessToken, detailFood);
+
+export default foodRouter;
