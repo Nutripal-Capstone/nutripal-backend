1c760fcc5bda8fd6361ae2c50db712a413a9f695
Rickyanto Wangsa Mulya <70902057+rickyantowm@users.noreply.github.com>
2023-06-13 19:10:15 +0700

    feat: handle date in server (#15)
    * feat: handle date in server
    
    * feat: add date

--- controllers/trackerController.js
+++ controllers/trackerController.js
@@ -1,26 +1,72 @@
-import { check, validationResult } from "express-validator";
-export const getTrackerData = [
-  check("date")
-    .notEmpty()
-    .withMessage("Date is required.")
-    .isISO8601()
-    .withMessage("Invalid date format."),
-  async (req, res) => {
-    const errors = validationResult(req);
+import moment from "moment-timezone";
 
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        success: false,
-        data: { errors: errors.array() },
-        message: "Validation errors.",
-      });
+export const getTrackerData = async (req, res) => {
+  let date = moment().tz("Asia/Jakarta").endOf("day").toDate();
+
+  const userId = req.user.id;
+  try {
+    const nutritionGoal = await prisma.nutritionGoal.findFirst({
+      where: {
+        userId: userId,
+        createdAt: {
+          lte: date,
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    //TODO: fetch food intake
+    const eatenFood = [];
+    const foodRecommendation = [];
+
+    let response = {
+      success: true,
+      data: {
+        date: date.toISOString().split("T")[0],
+        nutritionGoal: null,
+        foodRecommendation,
+        eatenFood,
+      },
+      message: "Success fetch tracker data.",
+    };
+
+    if (nutritionGoal) {
+      const { calorieGoal, proteinGoal, carbohydrateGoal, fatGoal } =
+        nutritionGoal;
+      response.data.nutritionGoal = {
+        calorieGoal,
+        proteinGoal,
+        carbohydrateGoal,
+        fatGoal,
+      };
     }
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      data: { error: error.message },
+      message: "Error fetching tracker data.",
+    });
+  }
+};
 
-    const date = new Date(req.query.date);
-    date.setHours(23, 59, 59, 999);
+export const getHistoryData = async (req, res) => {
+  let date = moment().tz("Asia/Jakarta").endOf("day").toDate();
 
-    const userId = req.user.id;
-    try {
+  const userId = req.user.id;
+  let response = {
+    success: true,
+    data: [],
+    message: "Success fetch history.",
+  };
+
+  try {
+    const page = parseInt(req.query.page) || 0;
+    date.setDate(date.getDate() - page * 7);
+
+    for (let i = 0; i < 7; i++) {
       const nutritionGoal = await prisma.nutritionGoal.findFirst({
         where: {
           userId: userId,
@@ -33,112 +79,32 @@ export const getTrackerData = [
         },
       });
 
-      //TODO: fetch food intake
-      const eatenFood = [];
-      const foodRecommendation = [];
-
-      let response = {
-        success: true,
-        data: {
-          nutritionGoal: null,
-          foodRecommendation,
-          eatenFood,
-        },
-        message: "Success fetch tracker data.",
-      };
-
       if (nutritionGoal) {
         const { calorieGoal, proteinGoal, carbohydrateGoal, fatGoal } =
           nutritionGoal;
-        response.data.nutritionGoal = {
-          calorieGoal,
-          proteinGoal,
-          carbohydrateGoal,
-          fatGoal,
-        };
-      }
-      return res.status(200).json(response);
-    } catch (error) {
-      return res.status(500).json({
-        success: false,
-        data: { error: error.message },
-        message: "Error fetching tracker data.",
-      });
-    }
-  },
-];
 
-export const getHistoryData = [
-  check("date")
-    .notEmpty()
-    .withMessage("Date is required.")
-    .isISO8601()
-    .withMessage("Invalid date format."),
-  async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        success: false,
-        data: { errors: errors.array() },
-        message: "Validation errors.",
-      });
-    }
-
-    let date = new Date(req.query.date);
-    date.setHours(23, 59, 59, 999);
-
-    const userId = req.user.id;
-    let response = {
-      success: true,
-      data: [],
-      message: "Success fetch history.",
-    };
-
-    try {
-      const page = parseInt(req.query.page) || 0;
-      date.setDate(date.getDate() - page * 7);
-
-      for (let i = 0; i < 7; i++) {
-        const nutritionGoal = await prisma.nutritionGoal.findFirst({
-          where: {
-            userId: userId,
-            createdAt: {
-              lte: date,
-            },
+        response.data.push({
+          date: date.toISOString().split("T")[0],
+          nutritionGoal: {
+            calorieGoal,
+            proteinGoal,
+            carbohydrateGoal,
+            fatGoal,
           },
-          orderBy: {
-            createdAt: "desc",
-          },
+          eatenFood: [],
         });
-
-        if (nutritionGoal) {
-          const { calorieGoal, proteinGoal, carbohydrateGoal, fatGoal } =
-            nutritionGoal;
-
-          response.data.push({
-            date: date.toISOString().split("T")[0],
-            nutritionGoal: {
-              calorieGoal,
-              proteinGoal,
-              carbohydrateGoal,
-              fatGoal,
-            },
-            eatenFood: [],
-          });
-        }
-
-        // Move to the previous day.
-        date.setDate(date.getDate() - 1);
       }
 
-      return res.status(200).json(response);
-    } catch (error) {
-      return res.status(500).json({
-        success: false,
-        data: { error: error.message },
-        message: "Error fetching tracker data.",
-      });
+      // Move to the previous day.
+      date.setDate(date.getDate() - 1);
     }
-  },
-];
+
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      data: { error: error.message },
+      message: "Error fetching tracker data.",
+    });
+  }
+};
