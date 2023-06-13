333c92ea40549f65a742b296104ee4898fa93ec4
Rickyanto Wangsa Mulya <rickyanto.wangsa@ui.ac.id>
2023-06-13 17:52:00 +0700

    feat: implement fetch history

--- controllers/trackerController.js
+++ controllers/trackerController.js
@@ -1,5 +1,4 @@
 import { check, validationResult } from "express-validator";
-
 export const getTrackerData = [
   check("date")
     .notEmpty()
@@ -18,7 +17,8 @@ export const getTrackerData = [
     }
 
     const date = new Date(req.query.date);
-    date.setDate(date.getDate() + 1);
+    date.setHours(23, 59, 59, 999);
+
     const userId = req.user.id;
     try {
       const nutritionGoal = await prisma.nutritionGoal.findFirst({
@@ -34,13 +34,15 @@ export const getTrackerData = [
       });
 
       //TODO: fetch food intake
-      const foodIntake = [];
+      const eatenFood = [];
+      const foodRecommendation = [];
 
       let response = {
         success: true,
         data: {
           nutritionGoal: null,
-          foodIntake,
+          foodRecommendation,
+          eatenFood,
         },
         message: "Success fetch tracker data.",
       };
@@ -55,6 +57,81 @@ export const getTrackerData = [
           fatGoal,
         };
       }
+      return res.status(200).json(response);
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        data: { error: error.message },
+        message: "Error fetching tracker data.",
+      });
+    }
+  },
+];
+
+export const getHistoryData = [
+  check("date")
+    .notEmpty()
+    .withMessage("Date is required.")
+    .isISO8601()
+    .withMessage("Invalid date format."),
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        success: false,
+        data: { errors: errors.array() },
+        message: "Validation errors.",
+      });
+    }
+
+    let date = new Date(req.query.date);
+    date.setHours(23, 59, 59, 999);
+
+    const userId = req.user.id;
+    let response = {
+      success: true,
+      data: [],
+      message: "Success fetch history.",
+    };
+
+    try {
+      const page = parseInt(req.query.page) || 0;
+      date.setDate(date.getDate() - page * 7);
+
+      for (let i = 0; i < 7; i++) {
+        const nutritionGoal = await prisma.nutritionGoal.findFirst({
+          where: {
+            userId: userId,
+            createdAt: {
+              lte: date,
+            },
+          },
+          orderBy: {
+            createdAt: "desc",
+          },
+        });
+
+        if (nutritionGoal) {
+          const { calorieGoal, proteinGoal, carbohydrateGoal, fatGoal } =
+            nutritionGoal;
+
+          response.data.push({
+            date: date.toISOString().split("T")[0],
+            nutritionGoal: {
+              calorieGoal,
+              proteinGoal,
+              carbohydrateGoal,
+              fatGoal,
+            },
+            eatenFood: [],
+          });
+        }
+
+        // Move to the previous day.
+        date.setDate(date.getDate() - 1);
+      }
+
       return res.status(200).json(response);
     } catch (error) {
       return res.status(500).json({
--- routes/trackerRoutes.js
+++ routes/trackerRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import verifyJWT from "../middleware/verifyJWT.js";
-import { getTrackerData } from "../controllers/trackerController.js";
+import { getTrackerData, getHistoryData } from "../controllers/trackerController.js";
 
 const trackerRouter = express.Router();
 
 trackerRouter.get("/", verifyJWT, getTrackerData);
+trackerRouter.get("/history", verifyJWT, getHistoryData);
 
 export default trackerRouter;
